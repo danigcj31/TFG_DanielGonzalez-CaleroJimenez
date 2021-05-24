e181a72db6219b82d4e4a13179da0e041c6081a4
danigcj31 <45356019+danigcj31@users.noreply.github.com>
2021-05-24 11:06:02 +0200

    Add files via upload

---
+++ src/main/java/backend/ContraseniasDistintasException.java
@@ -0,0 +1,13 @@
+package backend.excepciones;
+
+public class ContraseniasDistintasException {
+	/*
+	public ContraseniasDistintasException() {
+		// No se necesita hacer nada en el contructor
+	}
+
+	public String getMessage() {
+		return "Las contrasenas no coinciden";
+	}
+	*/
+}
